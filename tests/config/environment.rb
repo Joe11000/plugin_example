b65a218cc6baba91a08c9d95233fa25af4711023
Joe Noonan <joenoonan27@gmail.com>
2017-09-13 15:30:59 -0500

    added rspec

--- tests/config/environment.rb
+++ tests/config/environment.rb
@@ -3,3 +3,12 @@ require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+
+# Rails::Initializer.run do |config|
+#   # Point back to the plugin containing this rails_root
+#   config.plugin_paths = ["#{RAILS_ROOT}/../"]
+#   # Only load the plugin we're testing
+#   config.plugins = [ :plugin_example ]
+#   #...
+# end
